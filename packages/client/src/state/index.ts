05e85fdb36fcb693798804c7c68df88ec2715181
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-11-23 19:44:53 +0500

    rename state file

--- packages/client/src/state/index.ts
+++ packages/client/src/state/index.ts
@@ -1,1 +1,12 @@
-export * from "./draw";
+import { atom } from "jotai";
+import type { DrawMode } from "../shared/types";
+import type { GeoJSONStoreFeatures } from "terra-draw";
+
+export const drawModeAtom = atom<DrawMode>("static");
+export const featuresAtom = atom<GeoJSONStoreFeatures[]>([]);
+export const pointFeaturesAtom = atom<GeoJSONStoreFeatures[]>((get) => {
+  return get(featuresAtom).filter(
+    (feature) => feature.geometry.type === "Point"
+  );
+});
+export const createEventModalOpenAtom = atom<boolean>(false);
