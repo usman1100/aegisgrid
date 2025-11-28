de529fb029cc0e7a547b05a727cd10ebc5e0d6da
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-11-29 03:03:59 +0500

    polyfill bun CompressionStream dep

--- packages/server/drizzle.config.ts
+++ packages/server/drizzle.config.ts
@@ -1,5 +1,6 @@
 // drizzle.config.ts
 import { defineConfig } from "drizzle-kit";
+import "./polyfills/compression";
 
 if (!Bun.env.DATABASE_URL) throw new Error("DATABASE_URL is not defined");
 export default defineConfig({
