1e03acbd5ebb1d4bd530fa37d5e4710c880677e0
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-11-29 02:43:18 +0500

    make id uuid

--- packages/server/db/schema.ts
+++ packages/server/db/schema.ts
@@ -1,7 +1,7 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
+  id: uuid("id").primaryKey(),
   name: text("name").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
 });
