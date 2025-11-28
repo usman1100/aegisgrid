11115cff19588b4351e0875df6d07a9711dd8c02
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-11-29 03:03:11 +0500

    re gen migrations

--- packages/server/db/schema.ts
+++ packages/server/db/schema.ts
@@ -1,7 +1,7 @@
 import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: uuid("id").primaryKey(),
+  id: uuid("id").defaultRandom().primaryKey(),
   name: text("name").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
 });
--- packages/server/drizzle/0000_create_user.sql
+++ packages/server/drizzle/0000_create_user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text NOT NULL,
 	"created_at" timestamp DEFAULT now()
 );
