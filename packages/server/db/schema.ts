d7748c85150a5bc81010ad1af6fd830ae470554f
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-11-29 18:28:36 +0500

    add location on events

--- packages/server/db/schema.ts
+++ packages/server/db/schema.ts
@@ -1,8 +1,13 @@
-import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp, geometry } from "drizzle-orm/pg-core";
 
 export const events = pgTable("events", {
   id: uuid("id").defaultRandom().primaryKey(),
   name: text("name").notNull(),
+  location: geometry("location", {
+    type: "point",
+    mode: "xy",
+    srid: 4326,
+  }).notNull(),
   created_at: timestamp("created_at").defaultNow(),
   updated_at: timestamp("updated_at").defaultNow(),
 });
