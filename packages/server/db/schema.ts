856262672b30efa974a71d13ab16a1cedf7fef6e
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-11-29 15:56:20 +0500

    create events table

--- packages/server/db/schema.ts
+++ packages/server/db/schema.ts
@@ -1,2 +1,8 @@
 import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
 
+export const events = pgTable("events", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  created_at: timestamp("created_at").defaultNow(),
+  updated_at: timestamp("updated_at").defaultNow(),
+});
