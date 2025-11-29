9008b5f1cf66cde930af6192f6e26f933648c401
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2025-11-29 15:48:40 +0500

    remove user table

--- packages/server/db/schema.ts
+++ packages/server/db/schema.ts
@@ -1,7 +1,2 @@
 import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  name: text("name").notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
